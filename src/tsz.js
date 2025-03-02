55c7cfbf02164d4a3c1c5055cafa887abfd31400
moreshwarp <moreshwarpidadi@gmail.com>
2025-03-02 22:07:59 +0530

    done

--- src/tsz.js
+++ src/tsz.js
@@ -9,7 +9,8 @@
 
 console.log("Example 2");
 
-console.log(a); // Temporal Dead zone cant access before initilization but yes a and b both variable are hosited
+// console.log(a); 
+// Temporal Dead zone cant access before initilization but yes a and b both variable are hosited
 
 let a = 10; 
 console.log(b);
