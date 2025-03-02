4bb2b3f80eb8f275b02416d85d6b66645a527ac0
moreshwarp <moreshwarpidadi@gmail.com>
2025-03-02 22:16:20 +0530

    Hosting and TDZ

--- src/tsz.js
+++ src/tsz.js
@@ -9,11 +9,18 @@
 
 console.log("Example 2");
 
-// console.log(a); 
+// variable with let a is not avalaible to access its in TDZ 
+
+// console.log(a);  // not imitilized error but why?
+
 // Temporal Dead zone cant access before initilization but yes a and b both variable are hosited
 
-let a = 10; 
+let a = 10; // initilized 
+
+// variable with let a is not avalaible to access its in TDZ 
 console.log(b);
+
 var b = 100;
+
 console.log(b);
 
