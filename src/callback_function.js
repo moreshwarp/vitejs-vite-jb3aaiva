3ec9240ed2af2f680630f2bfbe50764f7baeedcc
moreshwarp <moreshwarpidadi@gmail.com>
2025-03-11 18:50:57 +0530

    Blocking the main thread

--- src/callback_function.js
+++ src/callback_function.js
@@ -31,7 +31,9 @@ console.log("What is callback function in JS?");
 
 // Let's deepdive using setTimeout function 
 
-setTimeout(function() {console.log("Timer");}, 5000);
+setTimeout(function () {
+  console.log("Timer");
+}, 5000);
 
 // setTimeOut function will be called and here the callback function is been passed as an argument to the setTimeOut also along with it after 5000ms the function will be called and print the value or whatever code is about to execute mean while this function be store in an seprate memory loaction and a timer of 5000ms will be attached to it and as JS is single threaded language 
 
@@ -49,3 +51,22 @@ x(function y() {
 // here the function is been called and argument to is been passed i.e. also a function i.e callback fucntion it will print the value for fucntion 
 
 // Hence, 1st x will be printed and then the value y will be printed and thne once the 5000ms are done the value Timer will be printed or logged on the console
+
+// in conclusion the setTimeOut asynchronous opration is not possible without the callback fucntion.
+// We gave the call back to setTimeOut and it executed after some time i.e 5000ms
+
+// if you see in the call stack the x and y are there in the call stack i.e. these functions are there in the callstack and once after the 5000ms are done the setTimeOut Function will be pop up in the call stack 
+
+
+// So now every execution that takes place in JS is using the Call Stack so when there any opertion that blocks the call stack then this is  know as blocking the main thread 
+
+
+// suppose your fucntion either x() or y()  have humangous line of code to execute so it would take muach time to execute the code and till that time other fucntions and code has to wait. 
+// And hence everything will be blocked 
+
+// till that time no other code or all the other code apart form x() and y() will have to wait for the execution as JS has only one CALL STACK and one MAIN THREAD it JS engine won't be able to execute the other code.
+
+
+//  As hence we say we should never block our main thread i.e. this CALL STACK 
+
+// Hence, we should alway take use of async operation given in the JavaScript, one of the example of SetTimeOut in order to prevent it from halting i.e. Bloacking the main thread i.e. CAll STACK i.e also know as main thread 
