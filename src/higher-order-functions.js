d292ce429a552a1c76bed7d036aa3081396d5aa8
moreshwarp <moreshwarpidadi@gmail.com>
2025-04-05 14:48:16 +0530

    Higher Order Function

--- src/higher-order-functions.js
+++ src/higher-order-functions.js
@@ -5,9 +5,18 @@ const radius = [3, 2, 5, 8];
 const calculateArea = function (radius) {
     const output = [];
     for (let i = 0; i < radius.length; i++){
-      output.push(Math.pi * radius[i] * radius[i]);
+      output.push(Math.PI * radius[i] * radius[i]);
+    }
+    return output;
+}
+
+const calculateCircumference = function (radius) {
+    const output = [];
+    for (let i = 0; i < radius.length; i++){
+      output.push(Math.PI * radius[i] * radius[i]);
     }
     return output;
 }
 
 console.log(calculateArea(radius));
+console.log(calculateCircumference(radius));
